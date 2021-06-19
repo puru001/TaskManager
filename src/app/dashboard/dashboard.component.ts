aa3179bddb095c3142245de6b0248bfd3dc72b42
p-GitHub1 <42931838+p-GitHub1@users.noreply.github.com>
2021-06-20 00:45:01 +1200

    dashboard design hard coded

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  Designation: string;
+  Username: string;
+  NoOfTeamMembers: number;
+  TotalCostOfAllProjects: number;
+  PendingTasks: number;
+  UpComingProjects: number;
+  ProjectCost: number;
+  CurrentExpenditure: number;
+  AvailableFunds: number;
+
+  Clients: string[];
+  Projects: string[];
+  Years: number[] = [];
+  TeamMembersSummary = [];
+  TeamMembers = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.Designation = 'Team Leader';
+    this.Username = 'Scott Smith';
+    this.NoOfTeamMembers = 67;
+    this.TotalCostOfAllProjects = 240;
+    this.PendingTasks = 15;
+    this.UpComingProjects = 2;
+    this.ProjectCost = 2113507;
+    this.CurrentExpenditure = 96788;
+    this.AvailableFunds = 52536;
   }
 
 }
